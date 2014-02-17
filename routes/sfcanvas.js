9fb31f573600e01719c98e46bb0d0379eef5ff07
khowling@gmail.com <khowling@gmail.com>
2014-02-17 21:38:52 +0000

    ok

--- routes/sfcanvas.js
+++ routes/sfcanvas.js
@@ -1,5 +1,8 @@
 module.exports = function (app, secret) {
 	
+	 var https = require('https')
+	  , url = require ('url');
+	
 	app.get('/go', function(req, res){
 		res.render ('index');
 	});
@@ -40,5 +43,55 @@ module.exports = function (app, secret) {
 
 		res.render('index', {title: 'hello', signedreq : json_envelope});
 	});
+	
+	
+	
+	/*** Doesn't Work, because all the relative URL resources in the page are not rewritten ***/
+	app.get('/proxy', function(proxyReq, proxyResp) {
+	    var params = url.parse(proxyReq.url, true);
+	    console.log ('got : ' + params.query.src);
+	    var URL = params.query.src;
+
+	    var destParams = url.parse(URL);
+
+	    var reqOptions = {
+	        host : destParams.host,
+	        port : 443,
+	        path : destParams.pathname + destParams.search,
+	        method : "GET"
+	    };
+
+	    console.log ('calling : ' + JSON.stringify( reqOptions));
+	    var req = https.request(reqOptions, function(res) {
+	        var headers = res.headers;
+	        headers['Access-Control-Allow-Origin'] = '*';
+	        headers['Access-Control-Allow-Headers'] = 'X-Requested-With';
+	        proxyResp.writeHead(200, headers);
+	        /* chunk will be a string, not a Buffer */
+	        res.setEncoding('utf8');
+
+	        res.on('data', function(chunk) {
+	        	console.log ('got data');
+	        	/* rewrite relative urls */
+	        	var rewrite_chunk = chunk.replace(/src="\//g, 'src="https://'+ destParams.host + '/');   
+	            proxyResp.write(rewrite_chunk);
+	        });
+
+	        res.on('end', function() {
+	        	console.log ('got end');
+	            proxyResp.end();
+	        });
+	    });
+
+	    req.on('error', function(e) {
+	        console.log('An error occured: ' + e.message);
+	        proxyResp.writeHead(503);
+	        proxyResp.write("Error!");
+	        proxyResp.end();
+	    });
+	    req.end();
+
+	});
+	/* **/
 	
 };
